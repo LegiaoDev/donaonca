8e2a427caeb5610a5224729efa55366e3a7f81c3
Gabriel <desenvolvimento@gadev.com.br>
2018-11-04 14:29:00 -0200

    Alterações na página de carrinho

--- wp-content/themes/donaonca/js/app.js
+++ wp-content/themes/donaonca/js/app.js
@@ -4,19 +4,26 @@ $(document).ready(()=> {
 	let descricao = document.querySelector('#tab-title-description a'),
 	avaliacao = document.querySelector('#tab-title-reviews a'),
 	areaAvaliacao = document.querySelector('#tab-reviews');
+	areaDescricao = document.querySelector('#tab-description');
 
 	
 	$(areaAvaliacao).hide();
 
 	
-	descricao.addEventListener('click', (e) => {
-		e.preventDefault();
-		alert('clicou');
-	});
+	if(descricao != null) {
+		descricao.addEventListener('click', (e) => {
+			e.preventDefault();
+			$(areaAvaliacao).hide();
+			$(areaDescricao).show();
+		});
+	}
 	
-	avaliacao.addEventListener('click', (e) => {
-		e.preventDefault();
-		alert('clicou');
-	});
+	if(avaliacao != null) {
+		avaliacao.addEventListener('click', (e) => {
+			e.preventDefault();
+			$(areaAvaliacao).show();
+			$(areaDescricao).hide();
+		});
+	}
 	
 });
